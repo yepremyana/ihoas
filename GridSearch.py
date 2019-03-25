cf27bc6ed9683f71f9f6e9676bb7871543835b48
Unknown <yepremyana@gmail.com>
2019-03-24 22:14:11 -0700

    changes to gridsearch for visual and saving

--- GridSearch.py
+++ GridSearch.py
@@ -3,20 +3,29 @@ import d3m.metadata.hyperparams as hyperparams
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
+import os
+current_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
+from pathlib import Path
+import csv
 import importlib
+from timeit import default_timer as timer
 
 class JPLGridSearch(object):
-    def __init__(self, primitive_class, data, target) -> None:
+    def __init__(self, primitive_class, data, target, dataset_name='') -> None:
         self.primitive_class = primitive_class
         self.data = data
         self.target = target
+        self.dataset_name = dataset_name
         self.parameters = {}
+        Path(current_dir + '/Results').mkdir(exist_ok=True, parents=True)
+        self.current_dir = current_dir + '/Results'
 
         primitive_json = primitive_class.metadata.query().get('name')
         import_module = ".".join(primitive_json.split(".")[:-1])
         sklearn_module = importlib.import_module(import_module)
-        import_class = primitive_json.split(".")[-1]
-        self.sklearn_class = getattr(sklearn_module, import_class)
+        self.import_class = primitive_json.split(".")[-1]
+        self.sklearn_class = getattr(sklearn_module, self.import_class)
+        self.out_file = self.retrieve_path()
 
     def _enumeration_to_config_space(self, name, hp_value):
         params_config = hp_value.values
@@ -113,13 +122,55 @@ class JPLGridSearch(object):
             param_grid.append(item)
 
         return param_grid
+
     def optimization(self):
         param_grid = self._get_hp_search_space()
+        print(param_grid)
         clf = GridSearchCV(self.sklearn_class(), param_grid, cv=5)
+        start = timer()
         clf.fit(self.data, self.target)
         print(sorted(clf.cv_results_['mean_test_score']))
-        y_pred = clf.predict(self.data)
-        print(y_pred)
+        # y_pred = clf.predict(self.data)
+        print("GridSearchCV took %.2f seconds for %d candidate parameter settings."
+              % (timer() - start, len(clf.cv_results_['params'])))
+        # print(y_pred)
+
+        # File to save first results
+        of_connection = open(self.out_file, 'w')
+        writer = csv.writer(of_connection)
+
+        # Write the headers to the file
+        writer.writerow(['loss', 'params', 'iteration', 'train_time'])
+        l = zip(clf.cv_results_['mean_test_score'], clf.cv_results_['params'],np.arange(1, len(clf.cv_results_['params'])+1), clf.cv_results_['mean_fit_time'])
+        for val in l:
+            writer.writerow(val)
+        of_connection.close()
+        # clf.score(data2_features, data2_target)
         print('The parameters combination that would give best accuracy is : ')
         print(clf.best_params_)
         print('The best accuracy achieved after parameter tuning via grid search is : ', clf.best_score_)
+        print(clf.cv_results_)
+
+    def _save_to_folder(self, path, savefig):
+        Path(self.current_dir + path).mkdir(exist_ok=True, parents=True)
+        return os.path.join(self.current_dir + path, savefig)
+
+    def retrieve_path(self):
+        return self._save_to_folder('/gridsearch_{}_{}'.format(self.import_class, self.dataset_name), 'Hyperparameter_Trials.csv')
+
+    def validate(self, test_data, test_target):
+        test_score = self.sklearn_class(*self.best_params).fit(self.data, self.target).score(test_data, test_target)
+
+        return test_score
+
+        #cv_results_
+        #score(X, y=None)
+        # New Model Evaluation metrics
+        # print('Accuracy Score : ' + str(accuracy_score(y_test, y_pred_acc)))
+        # print('Precision Score : ' + str(precision_score(y_test, y_pred_acc)))
+        # print('Recall Score : ' + str(recall_score(y_test, y_pred_acc)))
+        # print('F1 Score : ' + str(f1_score(y_test, y_pred_acc)))
+        #   r2_score
+        # # Logistic Regression (Grid Search) Confusion matrix
+        # confusion_matrix(y_test, y_pred_acc)
+        #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0, random_state=0)
