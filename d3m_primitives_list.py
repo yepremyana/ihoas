411ccf600665aaab7a5f55369c68670a14b9cc04
Unknown <yepremyana@gmail.com>
2019-04-07 18:24:28 -0700

    list of primitives to use

---
+++ d3m_primitives_list.py
@@ -0,0 +1,29 @@
+classification_primitives = [
+                                'd3m.primitives.classification.mlp.SKlearn',
+                                'd3m.primitives.classification.random_forest.SKlearn',
+                                'd3m.primitives.classification.linear_svc.SKlearn',
+                                'd3m.primitives.classification.extra_trees.SKlearn',
+                                'd3m.primitives.classification.gaussian_naive_bayes.SKlearn',
+                                'd3m.primitives.classification.bernoulli_naive_bayes.SKlearn',
+                                'd3m.primitives.classification.bagging.SKlearn',
+                                'd3m.primitives.classification.gradient_boosting.SKlearn',
+                                'd3m.primitives.classification.decision_tree.SKlearn',
+                                'd3m.primitives.classification.k_neighbors.SKlearn',
+                                'd3m.primitives.classification.svc.SKlearn'
+]
+
+regression_primitives = [
+                                'd3m.primitives.regression.ard.SKlearn',
+                                'd3m.primitives.regression.mlp.SKlearn',
+                                'd3m.primitives.regression.random_forest.SKlearn',
+                                'd3m.primitives.regression.ridge.SKlearn',
+                                'd3m.primitives.regression.linear_svr.SKlearn',
+                                'd3m.primitives.regression.extra_trees.SKlearn',
+                                'd3m.primitives.regression.gaussian_process.SKlearn',
+                                'd3m.primitives.regression.lars.SKlearn',
+                                'd3m.primitives.regression.lasso.SKlearn',
+                                'd3m.primitives.regression.decision_tree.SKlearn',
+                                'd3m.primitives.regression.k_neighbors.SKlearn',
+                                'd3m.primitives.regression.svr.SKlearn'
+
+]
