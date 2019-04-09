19dd2934bb875155da0b27118742185e9c9095ce
Unknown <yepremyana@gmail.com>
2019-04-08 19:31:58 -0700

    adding multiple runs for each dataset algo combo

--- Autogen_thesis.py
+++ Autogen_thesis.py
@@ -1,20 +1,24 @@
-import numpy as np
+import os
+current_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
+import argparse
+import csv
+
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+
 from d3m import index
+
+from HyperOpt import JPLHyperOpt
 from SMAC import JPLSMAC
 from BOHB import JPLBOHB
+from Hyperband import HB
+from Visual import Visual
+from Preprocessing import Preprocessing
 from datasets_id import classification_datasets_id, regression_datasets_id
 from d3m_primitives_list import classification_primitives, regression_primitives
-from Preprocessing import Preprocessing
-from Visual import Visual
-from HyperOpt import JPLHyperOpt
+
 import logging
 logging.basicConfig(level=logging.INFO)
-import csv
-rng = np.random.RandomState(0)
-import os
-import argparse
 
 def loop_through(args):
     if args.problem_type == 'classification':
@@ -27,34 +31,41 @@ def loop_through(args):
     for data_id in all_dataset_id:
         preprocess = Preprocessing(data_id = data_id)
         X_temp, y_temp = preprocess.simple_preprocessing()
-        X, y = shuffle(X_temp, y_temp, random_state=rng)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+        for i in range(3):
+            X, y = shuffle(X_temp, y_temp)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-        for primitive in all_primitives:
+            for primitive in all_primitives:
                 print(primitive)
                 primitive_obj = index.get_primitive(primitive)
                 # smac = JPLSMAC(primitive_obj, X_train, y_train, dataset_name=data_id, max_evals = 2)
                 # smac.optimization()
-                # all_visuals(smac)
-                # all_scores(smac, X_test, y_test, args.problem_type)
+                # if i == 0:
+                #     all_visuals(smac)
+                # all_scores(smac, X_test, y_test, args.problem_type, rerun = i)
                 # bohb = JPLBOHB(primitive_obj, X_train, y_train, dataset_name=data_id, max_evals = 2)
                 # bohb.optimization()
-                # all_visuals(smac)
-                # all_scores(bohb, X_test, y_test, args.problem_type)
-                hyperopt = JPLHyperOpt(primitive_obj, X_train, y_train, dataset_name=data_id, max_evals = 4)
+                # all_visuals(bohb)
+                # all_scores(bohb, X_test, y_test, args.problem_type, rerun = i)
+                # hb = HB(primitive_obj, X_train, y_train, dataset_name=data_id, max_evals = 2)
+                # hb.optimization()
+                # all_visuals(hb)
+                # all_scores(hb, X_test, y_test, args.problem_type, rerun = i)
+                hyperopt = JPLHyperOpt(primitive_obj, X_train, y_train, dataset_name=data_id, max_evals = 2, rerun = i)
                 hyperopt.optimization()
-                # all_visuals(hyperopt)
-                all_scores(hyperopt, X_test, y_test, args.problem_type)
+                # if i == 0:
+                #     all_visuals(hyperopt)
+                # all_scores(hyperopt, X_test, y_test, args.problem_type, rerun = i)
 
-def all_scores(algo, X_test, y_test, type_of_estimator):
-    outfile = open('Results/{}_scores.csv'.format(type_of_estimator), 'a')
+def all_scores(algo, X_test, y_test, type_of_estimator, rerun):
+    outfile = open('Results/{}_scores_{}.csv'.format(type_of_estimator, rerun), 'a')
     scores = algo.validate(X_test, y_test)
     writer = csv.DictWriter(outfile, scores.keys())
-    if os.stat('Results/{}_scores.csv'.format(type_of_estimator)).st_size == 0:
+    if os.stat('Results/{}_scores_{}.csv'.format(type_of_estimator, rerun)).st_size == 0:
         writer.writeheader()
     writer.writerow(scores)
     outfile.close()
-#add default to this
+
 def all_visuals(algo):
     path = algo.retrieve_path()
     visual = Visual(path)
@@ -62,11 +73,6 @@ def all_visuals(algo):
 
 if __name__ == "__main__":
     """
-     This script tries to use the first given argument as the primitive, the
-     second the dataset, the third is the benchmark.
-     python 'd3m.primitives.classification.random_forest.SKlearn' iris_data.csv iris_target.csv benchmark.py
-     
-     - defaults can also be set
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--problem_type", help="Problem type that is to be run", required=True)
@@ -78,14 +84,3 @@ if __name__ == "__main__":
 # compare the validation test scores across all and pick best one
 # some sort choose the best param at 1 hr 2 hr 3hr and by evals
 # add code for metadata datasets
-
-# clean up code
-# figure out how to run 5 random times
-# where to save files
-# make hyperband
-# try and except in hyperband
-# adding time since start
-# add a time to stop and see how stopping is done in hpolib
-# translate hyperopt and bohb
-# fix hypdfopt in validation
-# fix bohb in best_params
